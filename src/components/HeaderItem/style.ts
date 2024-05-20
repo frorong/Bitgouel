b4026fe84838edfbfc655d95ea59d45af123ad67
Lee-Seungje <moondgod@gmail.com>
2024-05-20 22:44:32 +0900

    Add hover style

--- src/components/HeaderItem/style.ts
+++ src/components/HeaderItem/style.ts
@@ -8,9 +8,22 @@ export const Wrapper = styled(Link)`
   justify-content: center;
 
   width: 10.5rem;
+  height: 100%;
+
+  transition: ease-in-out 0.2s;
+  border-bottom: ${({ theme }) => theme.color.white} 2px solid;
+
+  :hover {
+    border-bottom: ${({ theme }) => theme.color.secondary} 1px solid;
+    span {
+      color: ${({ theme }) => theme.color.secondary};
+    }
+  }
 `;
 
 export const ItemTitle = styled.span`
   ${({ theme }) => theme.typo.subtitle};
   color: ${({ theme }) => theme.color.grey[900]};
+
+  transition: ease-in-out 0.2s;
 `;
