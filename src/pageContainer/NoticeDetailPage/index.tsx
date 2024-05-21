986eec3890a0ff8c52fbcb64ba0551c43e29ba12
Lee-Seungje <moondgod@gmail.com>
2024-05-21 11:41:59 +0900

    Add returnButton

--- src/pageContainer/NoticeDetailPage/index.tsx
+++ src/pageContainer/NoticeDetailPage/index.tsx
@@ -3,13 +3,25 @@
 import { NotionRenderer } from "react-notion-x";
 import { ExtendedRecordMap } from "notion-types";
 
+import { ReturnChevronIcon } from "@/assets";
+
+import * as S from "./style";
+
+const NOTICE_LIST_PATH = "/notice" as const;
+
 interface Props {
   recordMap: ExtendedRecordMap;
 }
 
 const NoticeDetailPage: React.FC<Props> = ({ recordMap }) => {
   return (
-    <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
+    <S.Wrapper>
+      <S.ReturnButton href={NOTICE_LIST_PATH}>
+        <ReturnChevronIcon />
+        <S.ReturnText>뒤로가기</S.ReturnText>
+      </S.ReturnButton>
+      <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
+    </S.Wrapper>
   );
 };
 
---
+++ src/pageContainer/NoticeDetailPage/style.ts
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+import Link from "next/link";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+
+  width: 100vw;
+  padding: 1.25rem 6.25rem;
+`;
+
+export const ReturnButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 0.625rem;
+
+  width: fit-content;
+  padding: 0.625rem;
+  border-radius: 0.5rem;
+  background-color: ${({ theme }) => theme.color.secondary};
+`;
+
+export const ReturnText = styled.span`
+  ${({ theme }) => theme.typo.button};
+  color: ${({ theme }) => theme.color.black};
+`;
