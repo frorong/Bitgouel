44add4f0dd0c0d7b20b5da70ebb6d1a0c8a92646
Lee-Seungje <moondgod@gmail.com>
2024-05-21 11:31:37 +0900

    Update to server

--- src/app/notice/[id]/page.tsx
+++ src/app/notice/[id]/page.tsx
@@ -1,15 +1,18 @@
 import "react-notion-x/src/styles.css";
 
 import { NoticeDetailPage } from "@/pageContainer";
+import { notion } from "@/lib";
+import { notice } from "@/constant";
 
+import { redirect } from "next/navigation";
 import type { Metadata } from "next";
-
-import { NotionAPI } from "notion-client";
 
 export const metadata: Metadata = {
   title: "빛고을배드민턴클럽 홈패이지",
   description: "빛고을배드민턴클럽의 홈패이지입니다.",
 };
+
+const NOTICE_LIST_PATH = "/notice" as const;
 
 interface Params {
   params: {
@@ -17,6 +20,11 @@ interface Params {
   };
 }
 
-export default function Notice({ params: { id } }: Params) {
-  return <NoticeDetailPage id={id} />;
+export default async function Notice({ params: { id } }: Params) {
+  const currentNotice = notice[parseInt(id)];
+
+  if (!currentNotice) return redirect(NOTICE_LIST_PATH);
+
+  const recordMap = await notion.getPage(currentNotice.notionLink);
+  return <NoticeDetailPage recordMap={recordMap} />;
 }
--- src/pageContainer/NoticeDetailPage.tsx
+++ src/pageContainer/NoticeDetailPage.tsx
@@ -1,52 +1,15 @@
 "use client";
 
 import { NotionRenderer } from "react-notion-x";
-import { NotionAPI } from "notion-client";
 import { ExtendedRecordMap } from "notion-types";
 
-import { useRouter } from "next/navigation";
-
-import { notice } from "@/constant";
-
-import { useEffect, useState } from "react";
-
 interface Props {
-  id: string;
+  recordMap: ExtendedRecordMap;
 }
 
-const NOTICE_LIST_PATH = "/notice" as const;
-
-const getMap = async (notionLink: string) => {
-  const notion = new NotionAPI();
-  const recordMap = await notion.getPage(notionLink);
-
-  return recordMap;
-};
-
-const NoticeDetailPage: React.FC<Props> = ({ id }) => {
-  const { push } = useRouter();
-
-  const [map, setMap] = useState<ExtendedRecordMap>();
-
-  const currentNotice = notice[parseInt(id)];
-
-  if (!currentNotice) push(NOTICE_LIST_PATH);
-
-  const setNewMap = async () => {
-    const newMap = await getMap(currentNotice.notionLink);
-    setMap(newMap);
-  };
-
-  useEffect(() => {
-    setNewMap();
-  }, []);
-
+const NoticeDetailPage: React.FC<Props> = ({ recordMap }) => {
   return (
-    <>
-      {map && (
-        <NotionRenderer recordMap={map} fullPage={true} darkMode={false} />
-      )}
-    </>
+    <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
   );
 };
 
