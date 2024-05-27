687b0c82e3d2fd1e1a9b8595057495a3ec90d041
Lee-Seungje <moondgod@gmail.com>
2024-05-27 15:05:45 +0900

    Add page

--- src/pageContainer/index.ts
+++ src/pageContainer/index.ts
@@ -1,4 +1,6 @@
 export { default as MainPage } from "./MainPage";
 export { default as NoticePage } from "./NoticePage";
 export { default as NoticeDetailPage } from "./NoticeDetailPage";
+export { default as GalleryPage } from "./GalleryPage";
+export { default as GalleryDetailPage } from "./GalleryDetailPage";
 export { default as InquiryPage } from "./InquiryPage";
