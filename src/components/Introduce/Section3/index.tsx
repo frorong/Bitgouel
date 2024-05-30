f1601fb4d156b64c6e1e222b59b2ef09da831a46
Lee-Seungje <moondgod@gmail.com>
2024-05-30 23:30:26 +0900

    Add ledgend

--- src/components/Introduce/Section3/index.tsx
+++ src/components/Introduce/Section3/index.tsx
@@ -1,25 +1,103 @@
 "use client";
 
 import * as S from "./style";
-
+import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useIntersectionObserver } from "@/hooks";
 
 interface Props {
   forwardRef?: any;
 }
 
 const Section3: React.FC<Props> = ({ forwardRef }) => {
-  gsap.registerPlugin(ScrollTrigger);
-  const setItems = () => {
-    gsap.to("#i1", { duration: 1, x: 350, ease: "elastic", opacity: 1 });
-    gsap.to("#i2", { duration: 1, x: -350, ease: "elastic", opacity: 1 });
-    gsap.to("#i3", { duration: 3, y: 670, ease: "elastic", opacity: 1 });
-    gsap.to("#i4", { duration: 2, x: -480, ease: "elastic", opacity: 1 });
-    gsap.to("#i5", { duration: 2, x: 480, ease: "elastic", opacity: 1 });
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  const moveElements = (isUp: boolean) => {
+    gsap.to("#content1", {
+      scrollTrigger: "#content1",
+      y: isUp ? -400 : 0,
+      duration: 4,
+    });
+
+    gsap.to("#content2", {
+      scrollTrigger: "#content2",
+      y: isUp ? -300 : 0,
+      duration: 4,
+    });
+
+    gsap.to("#content3", {
+      scrollTrigger: "#content3",
+      y: isUp ? -200 : 0,
+      duration: 4,
+    });
   };
 
-  return <S.Section ref={forwardRef}></S.Section>;
+  const handleIntersection: IntersectionObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        moveElements(true);
+      } else {
+        moveElements(false);
+      }
+    });
+  };
+
+  const { observe, unobserve } = useIntersectionObserver(handleIntersection, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  });
+
+  useEffect(() => {
+    const target = targetRef.current;
+    if (target) {
+      observe(target);
+    }
+
+    return () => {
+      if (target) {
+        unobserve(target);
+      }
+    };
+  }, [observe, unobserve]);
+
+  return (
+    <S.Section ref={forwardRef}>
+      <S.Half />
+      <S.TitleWrapper>
+        <S.Title>빛고을 배드민턴 클럽은 이런 가치를 추구해요!</S.Title>
+      </S.TitleWrapper>
+      <S.ContentWrapper ref={targetRef}>
+        <S.Content id="content1">
+          <S.ContentHead color="pink">
+            <S.ContentTitle>함께 어울리는 분위기</S.ContentTitle>
+          </S.ContentHead>
+          <S.ContentDescription>
+            운동와서 게임못하고 있는 회원이 있으면 같이 어울려서 게임할수 있도록
+            주위에서 같이 이끌어주시고 소외되는 부분이 없도록 협조부탁드립니다.
+          </S.ContentDescription>
+        </S.Content>
+        <S.Content id="content2">
+          <S.ContentHead color="pink">
+            <S.ContentTitle>회원간에 인사잘하기</S.ContentTitle>
+          </S.ContentHead>
+          <S.ContentDescription>
+            인사는 인간관계의 시작이자 기본입니다. 밝은 인사를 통해 긍정적인
+            이미지를 쌓아갈 수 있고 상호 관계도 더욱 돈독해질 수 있습니다.
+          </S.ContentDescription>
+        </S.Content>
+        <S.Content id="content3">
+          <S.ContentHead color="pink">
+            <S.ContentTitle>클럽 회원 우대</S.ContentTitle>
+          </S.ContentHead>
+          <S.ContentDescription>
+            외부인 방문시 클럽회원이 많은경우 외부인은 가급적 4.5.6 코트에서
+            게임하도록 유도 부탁 드립니다.
+          </S.ContentDescription>
+        </S.Content>
+      </S.ContentWrapper>
+    </S.Section>
+  );
 };
 
 export default Section3;
