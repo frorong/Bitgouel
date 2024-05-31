2a381482d3f5af777d8475fc396c3c7c19db7eb6
Lee-Seungje <moondgod@gmail.com>
2024-05-31 09:03:43 +0900

    Add text

--- src/components/Introduce/Section4/index.tsx
+++ src/components/Introduce/Section4/index.tsx
@@ -9,6 +9,17 @@ interface Props {
 const Section4: React.FC<Props> = ({ forwardRef }) => (
   <S.Section ref={forwardRef}>
     <S.Half />
+
+    <S.TelWrapper>
+      <S.Title>빛고을 배드민턴 클럽에 가입하려면?</S.Title>
+
+      <S.TelContent>
+        <S.TelText>총무님: 010-1234-5678</S.TelText>
+      </S.TelContent>
+      <S.TelContent></S.TelContent>
+      <S.TelText>회장님: 010-1234-5678</S.TelText>
+    </S.TelWrapper>
+    <a href="/Introduce/rule.hwp" download></a>
   </S.Section>
 );
 
--- src/components/Introduce/Section4/style.ts
+++ src/components/Introduce/Section4/style.ts
@@ -19,8 +19,45 @@ export const Half = styled.div`
   border-radius: 0 0 40% 0;
 
   background: linear-gradient(
-    to right,
-    ${({ theme }) => theme.color.secondary},
-    ${({ theme }) => theme.color.white}
-  );
+      to right,
+      ${({ theme }) => theme.color.secondary},
+      rgba(251, 226, 222, 0)
+    ),
+    url("/Introduce/cover.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+export const Title = styled.span`
+  font-size: 3.75rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.grey[900]};
+
+  margin-top: 1.25rem;
+  z-index: 1;
+`;
+
+export const TelWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  z-index: 1;
+  width: 100vw;
+  padding-left: 100px;
+
+  padding-top: calc(50vh);
+`;
+
+export const TelContent = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;
+
+export const TelText = styled.span`
+  font-size: 2.5rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.grey[900]};
 `;
