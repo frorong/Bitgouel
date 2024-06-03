f1e2bfca9d29a8bbc0944ba7dbebab967e11a41a
Lee-Seungje <moondgod@gmail.com>
2024-06-03 17:55:10 +0900

    Hotfix delete link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <head>
-        <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin=""
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
-        />
-      </head>
       <body>
         <Providers>
           <Header />
