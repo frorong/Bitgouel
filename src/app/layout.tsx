c8e73733f49b71f36c1060bf2cf8d278cc244291
Lee-Seungje <moondgod@gmail.com>
2024-06-03 18:12:57 +0900

    Update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { GoogleAnalytics } from "@/components";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "빛고을배드민턴클럽 홈패이지",
-  description: "빛고을배드민턴클럽의 홈패이지입니다.",
+  title: "빛고을배드민턴클럽 홈페이지",
+  description: "빛고을배드민턴클럽의 홈페이지입니다.",
 };
 
 export default function RootLayout({
