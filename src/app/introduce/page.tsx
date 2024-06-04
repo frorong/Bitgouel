10a50c271169c6689e9b686915a5be95f122d364
Lee-Seungje <moondgod@gmail.com>
2024-06-04 19:37:31 +0900

    Update introduce

--- src/app/introduce/page.tsx
+++ src/app/introduce/page.tsx
@@ -1,5 +1,12 @@
 import { IntroducePage } from "@/pageContainer";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "소개 | Bitguoel",
+  description: "빛고을배드민턴클럽의 소개 페이지입니다.",
+};
+
 export default async function Introduce() {
   return <IntroducePage />;
 }
