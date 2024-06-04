294aac6cde9ba2556389b2b7df2887470cdb274a
Lee-Seungje <moondgod@gmail.com>
2024-06-04 19:37:04 +0900

    Update inquiry

--- src/app/inquiry/page.tsx
+++ src/app/inquiry/page.tsx
@@ -1,7 +1,13 @@
-import { cookies } from "next/headers";
-
 import { InquiryPage } from "@/pageContainer";
+
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "문의 | Bitguoel",
+  description: "빛고을배드민턴클럽의 문의하기 페이지입니다.",
+};
 
 export default async function Inquiry() {
   const isMailed = cookies().get("isMailed")?.value;
