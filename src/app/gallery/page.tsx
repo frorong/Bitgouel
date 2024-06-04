e3f47548dbd2f6c538a8361fbc634f1139ac5dcd
Lee-Seungje <moondgod@gmail.com>
2024-06-04 19:38:56 +0900

    Update gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,12 @@
 import { NotionPage } from "@/pageContainer";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "갤러리 | Bitguoel",
+  description: "빛고을배드민턴클럽의 행사갤러리 페이지입니다.",
+};
+
 export default function Gallery() {
   return <NotionPage postType="gallery" />;
 }
